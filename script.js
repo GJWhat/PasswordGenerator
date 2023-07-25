d0361fbc3e9a890201c091567b4af0cf8b940541
GJWhat <Gavwatton@hotmail.com>
2023-07-25 18:42:40 +0100

    added Random

--- script.js
+++ script.js
@@ -130,7 +130,10 @@ return {
 };
 
 // Function for getting a random element from an array
-function getRandom(arr) {}
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
 
 // Function to generate password with user input
 function generatePassword() {}
